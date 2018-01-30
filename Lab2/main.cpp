afc3854da3e8c0f29399b7756c1bc90e91558480
Tilak N Shenoy <tilaknshenoy@gmail.com>
2018-01-30 11:52:53 +0530

    Lab2

--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -3,16 +3,27 @@
 #include <stdio.h>
 
 void Bresenhams(int x1,int y1,int x2,int y2){
-    mNew=2*(y2-y1);
-    slopeErr=mNew-(x2-x1);
-
+    int mNew=2*(y2-y1);
+    int dy=y2-y1;
+    int dx=x2-x1;
+    int slopeErr=mNew-dx;
+    glBegin(GL_POINTS);
+    for(int x=x1,y=y1;x<=x2;x++){
+        glVertex2i(x,y);
+        slopeErr+=mNew;
+        if( slopeErr>=0){
+            y++;
+            slopeErr-=2*dx;
+        }
+    }
+    glEnd();
 }
 
 void display()
 {
 glClear(GL_COLOR_BUFFER_BIT);
-glColor3b(1.0,0,0);
-//DDA(-300.2,200.4,200.3,150);
+glColor3b(1.0,1.0,0);
+Bresenhams(50,250,280,500);
 glutSwapBuffers();
 }
 void reshape(int w, int h)
@@ -21,7 +32,8 @@ glViewport(0.0, 0.0, w, h);
 }
 void intiopenGL()
 {
-glClearColor(1.0, 1.0, 1.0, 1.0);
+glClearColor(1.0, 1.0, 0, 1.0);
+glOrtho(0,500,0,500,0,1);
 }
 
 int main(int argc, char** argv)
